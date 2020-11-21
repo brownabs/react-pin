b44a30784c0e78ec46f0a2f9340be53811fd244e
Abbey Brown <abbeybrownsemail@gmail.com>
2020-11-21 10:12:45 -0600

    class psuedo code

--- src/helpers/Routes.js
+++ src/helpers/Routes.js
@@ -11,6 +11,7 @@ import SearchResults from '../views/SearchResults';
 // The PrivateRoute function is creating a private route and returing the specified route based on the props
 
 // We specify the specific props we want to use in the routeChecker and pass the rest with the spread
+// could pass just props in argument but then would have to use dot notation, so we are destructuring
 const PrivateRoute = ({ component: Component, user, ...rest }) => {
   // when we call this function in the return, it is looking for an argument. `props` here is taco.
   const routeChecker = (taco) => (user
@@ -21,6 +22,18 @@ const PrivateRoute = ({ component: Component, user, ...rest }) => {
   // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
   return <Route {...rest} render={(props) => routeChecker(props)} />;
 };
+
+// example of how you can use the route function and specify props
+// const SuperPrivateRoute = ({ component: Component, user, ...rest }) => {
+//   // when we call this function in the return, it is looking for an argument. `props` here is taco.
+//   const routeChecker = (taco) => (user.admin
+//     ? (<Component {...taco} user={user} />)
+//     : (<Redirect to={{ pathname: '/super-private', state: { from: taco.location } }} />));
+//     // this render method is one we can use instead of component. Since the components are being dynamically created, we use render. Read the docs for more info: https://reactrouter.com/web/api/Route/render-func
+
+//   // Just like in the routes if we want the dynamically rendered component to have access to the Router props, we have to pass `props` as an argument.
+//   return <Route {...rest} render={(props) => routeChecker(props)} />;
+// };
 
 export default function Routes({ user }) {
   return (
