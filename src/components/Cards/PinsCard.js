aae7962699b875af68e36522055a9684a5a31da5
Abbey Brown <abbeybrownsemail@gmail.com>
2020-12-08 18:22:30 -0600

    search input complete and terinary for no results

--- src/components/Cards/PinsCard.js
+++ src/components/Cards/PinsCard.js
@@ -28,7 +28,7 @@ export default class PinCard extends Component {
   render() {
     const { pin, detailsPage } = this.state;
     return (
-    <div className='card m-2 h-50 w-25'>
+    <div className='card m-1 h-25 w-25'>
       <img className='card-img-top' src={pin.imgUrl} alt='Card cap' />
       <div className='card-body'>
         <h5 className='card-title'>{pin.name}</h5>
--- src/views/SearchResults.js
+++ src/views/SearchResults.js
@@ -1,54 +1,60 @@
 import React, { Component } from 'react';
 import BoardsCard from '../components/Cards/BoardsCard';
 import PinsCard from '../components/Cards/PinsCard';
-// import SearchBoards from '../helpers/data/boardData';
+import { getAllUserBoards } from '../helpers/data/boardData';
+import { getAllUserPins } from '../helpers/data/pinData';
+import getUid from '../helpers/data/authData';
 
 export default class SearchResults extends Component {
-  state={
+  state = {
     results: [],
     searchTerm: '',
     searchType: '',
   }
 
-  // componentDidMount() {
-  //   this.performSearch();
-  // }
+  componentDidMount() {
+    this.performSearch();
+  }
 
-  // performSearch = () => {
-  //   const searchTerm = this.props.match.params.term;
-  //   const searchType = this.props.match.params.type;
+  performSearch = () => {
+    const searchTerm = this.props.match.params.term.toLowerCase();
+    const searchType = this.props.match.params.type;
+    const currentUser = getUid();
 
-  //   if (searchType === 'boards') {
-  //     // api calls that gets boards with search term .filter()
-  //     SearchBoards()
-  //       .then((response) => {
-  //         console.warn(response);
-  //       });
-  //     this.setState({
-  //       // api results will update results in state
-  //       searchTerm,
-  //       searchType,
-  //     });
-  //   } else {
-  //     this.setState({
-  //       searchTerm,
-  //       searchType,
-  //     });
-  //   }
-  // }
+    if (searchType === 'boards') {
+      getAllUserBoards(currentUser).then((boardsArray) => {
+        const boardResults = boardsArray.filter((board) => board.name.toLowerCase().includes(searchTerm) || board.description.toLowerCase()).includes(searchTerm);
+        this.setState({
+          searchType,
+          searchTerm,
+          results: boardResults,
+        });
+      });
+    } else {
+      getAllUserPins(currentUser).then((pinsArray) => {
+        const pinResults = pinsArray.filter((pin) => pin.name.toLowerCase().includes(searchTerm) || pin.description.toLowerCase().includes(searchTerm));
+        this.setState({
+          searchTerm,
+          searchType,
+          results: pinResults,
+        });
+      });
+    }
+  }
 
-  // componentDidUpdate(prevProps, prevState) {
-  //   if (prevState.searchTerm !== this.props.match.params.term) {
-  //     this.performSearch();
-  //   }
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.searchTerm !== this.props.match.params.term) {
+      this.performSearch();
+    }
+  }
 
   render() {
     const { results, searchType } = this.state;
-
+    console.warn(results);
     const showResults = () => (
-      results.map((result) => (
-        searchType === 'boards' ? <BoardsCard key={result.firebaseKey} board={result}/> : <PinsCard key={result.firebaseKey} pin={result}/>))
+      results.length ? results.map((result) => (
+        searchType === 'boards' ? <BoardsCard key={result.firebaseKey} board={result}/> : <PinsCard key={result.firebaseKey} pin={result}/>
+      )) : <h1>There are no {searchType} to view</h1>
     );
     return (
       <div>
